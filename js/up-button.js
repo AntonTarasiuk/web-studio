d8e08172f04d625daa42ffa0ef9d6c84cb98d0d2
AntonTarasiuk <tarasiuk.anton@gmail.com>
2023-01-16 18:55:55 +0200

    Adapted full index and portfolio pages

--- js/up-button.js
+++ js/up-button.js
@@ -1,8 +1,9 @@
 const upButton = document.querySelector("#up-button")
+const headerHeight = document.querySelector(".header").offsetHeight
 
 window.onscroll = () => { 
     upButton.style.display = 
-        document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 
+        document.body.scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight 
         ? "block" 
         : "none";
 };
